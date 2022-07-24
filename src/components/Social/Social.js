e894fce9a981b170a57414fd6b2b269d1318997b
Begli Kakabalov <begli.kaka@mail.ru>
2022-07-24 22:37:04 +0200

    added a tag to social component

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -1,26 +1,25 @@
-import React from "react";
-import "./Social.css";
-import { Link } from "react-router-dom";
+import React from 'react'
+import './Social.css'
 
 const Social = () => {
   return (
-    <div className=" social">
-      <div className="social-items">
-        <Link to="https://github.com/Begli0102">
-          <i className="fab fa-github"></i>
-        </Link>
-        <Link to="https://linkedin.com/in/beglikakabalov">
-          <i className="fab fa-linkedin-in"></i>
-        </Link>
-        <Link to="#">
-          <i className="fab fa-twitter"></i>
-        </Link>
-        <Link to="#">
-          <i class="fab fa-medium-m"></i>
-        </Link>
+    <div className='social'>
+      <div className='social-items'>
+        <a href='https://github.com/Begli0102'>
+          <i className='fab fa-github'></i>
+        </a>
+        <a href='https://linkedin.com/in/beglikakabalov'>
+          <i className='fab fa-linkedin-in'></i>
+        </a>
+        <a href='#'>
+          <i className='fab fa-twitter'></i>
+        </a>
+        <a href='#'>
+          <i class='fab fa-medium-m'></i>
+        </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Social;
+export default Social
